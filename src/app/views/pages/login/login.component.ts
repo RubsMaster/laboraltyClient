53c7120bc947c9cd2965996d6fb9b6e7c458f47b
Hugo <huedlup@gmail.com>
2023-05-27 12:04:49 -0700

    cambios login y usuarios jalando

--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 // import Validation from './utils/validation';
+import { AuthService } from "../../../services/auth/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,14 @@ import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/fo
 })
 export class LoginComponent {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void{
+    const adminData = {
+      userName: 'admin',
+      password: 'admin'
+    };
+    this.authService.login(adminData).subscribe((res) => console.log('login'));
+  }
 
 }
