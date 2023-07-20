e76d97dbbe509ec0d9b721480c8931fda257f721
rubsmaster <jrubsb@gmail.com>
2023-07-20 17:17:43 -0600

    some changes in service log need to put who create

--- src/app/views/accountant/service-log/service-log.component.ts
+++ src/app/views/accountant/service-log/service-log.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import {
   FormControl,
   FormBuilder,
   FormGroup,
-  Validators
+  Validators,
 } from "@angular/forms";
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from "@angular/router";
 import { tasks } from "../../../models/tasks";
 import { ServiceLog } from "../../../models/serviceLog";
 import { ServiceLogService } from "../../../services/accountant/service-log/service-log.service";
 
-import { cilLowVision, cilCircle, cilCheckCircle, cilLockUnlocked, cilLockLocked, cilFingerprint } from "@coreui/icons";
+import {
+  cilLowVision,
+  cilCircle,
+  cilCheckCircle,
+  cilLockUnlocked,
+  cilLockLocked,
+  cilFingerprint,
+} from "@coreui/icons";
 
 @Component({
-  selector: 'app-service-log',
-  templateUrl: './service-log.component.html',
-  styleUrls: ['./service-log.component.scss']
+  selector: "app-service-log",
+  templateUrl: "./service-log.component.html",
+  styleUrls: ["./service-log.component.scss"],
 })
-
 export class ServiceLogComponent implements OnInit {
   icons = {
     cilLowVision,
@@ -25,68 +31,72 @@ export class ServiceLogComponent implements OnInit {
     cilCheckCircle,
     cilLockLocked,
     cilLockUnlocked,
-    cilFingerprint
+    cilFingerprint,
   };
 
   serviceLogForm: FormGroup;
 
-   taskList: tasks[] = []
+  serviceLogName: string;
+  servicceLogType: string;
+  taskName: string;
 
-   serviceLogList: ServiceLog[] = []
+  taskList: tasks[] = [];
 
-    //pagination 
-    paginationId = 'consultantPagination';
-    p1: number = 1;
-    itemsPerPage:number = 5;
-    currentPage = 1;
+  serviceLogList: ServiceLog[] = [];
+
+  //pagination
+  paginationId = "consultantPagination";
+  p1: number = 1;
+  itemsPerPage: number = 5;
+  currentPage = 1;
 
   constructor(
     private formBuilder: FormBuilder,
     private service: ServiceLogService
   ) {
     this.serviceLogForm = this.formBuilder.group({
-      serviceLogName: new FormControl('', Validators.required),
-      serviceLogType: new FormControl('', Validators.required),
-      taskName: new FormControl('', Validators.required)
-    })
-   }
+      serviceLogName: new FormControl("", Validators.required),
+      serviceLogType: new FormControl("", Validators.required),
+      taskName: new FormControl("", Validators.required),
+    });
+    this.serviceLogName = 'Nombre'
+    this.servicceLogType = 'Tipo de documento'
+    this.taskName = 'Nombre tarea: '
+  }
 
   ngOnInit(): void {
-    this.getAllServiceLogs()
+    this.getAllServiceLogs();
   }
 
-  addTaskToList(taskNameParameter: string){
+  addTaskToList(taskNameParameter: string) {
     const task: tasks = {
       name: taskNameParameter,
-      completed: false
-    }
-     this.taskList.push(task)
+      completed: false,
+    };
+    this.taskList.push(task);
   }
 
-  deleteTask(name: string, index: number){
+  deleteTask(name: string, index: number) {
     if (index >= 0 && index < this.taskList.length) {
       this.taskList.splice(index, 1);
     }
   }
 
-  getAllServiceLogs(){
-    this.service.getAllServiceLogs().subscribe( (data: any) => {
+  getAllServiceLogs() {
+    this.service.getAllServiceLogs().subscribe((data: any) => {
       this.serviceLogList = data as ServiceLog[];
-      this.serviceLogList.reverse()
-    })
+      this.serviceLogList.reverse();
+    });
   }
 
   saveService() {
-    
-     const serviceLog: ServiceLog = {
-       serviceLogName: this.serviceLogForm.get('serviceLogName')?.value,
-       serviceLogType: this.serviceLogForm.get('serviceLogType')?.value,
-       tasks: this.taskList
-     }
-     this.service.createServiceLog(serviceLog).subscribe(data => {
-      console.log("esto es lo que se creó: " + JSON.stringify(data))
-     })
- }
-
-
+    const serviceLog: ServiceLog = {
+      serviceLogName: this.serviceLogForm.get("serviceLogName")?.value,
+      serviceLogType: this.serviceLogForm.get("serviceLogType")?.value,
+      tasks: this.taskList,
+    };
+    this.service.createServiceLog(serviceLog).subscribe((data) => {
+      console.log("esto es lo que se creó: " + JSON.stringify(data));
+    });
+  }
 }
